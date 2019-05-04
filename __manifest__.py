8ffb591c23d8feffcb567bb346e7ffa0783c5cd0
Ivan <ivan@cetmix.com>
2019-05-04 16:43:31 +0300

    Updates

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Open PDF Reports and PDF Attachments in Browser',
+    'name': 'Preview Report PDF in Browser',
     'version': '10.0.1.0',
-    'summary': """Open PDF Reports and PDF Attachments in Browser""",
+    'summary': """Preview PDF Reports in Browser""",
     'author': 'Ivan Sokolov, Ahmed Khakwani',
     'category': 'Productivity',
     'license': 'GPL-3',
     'website': 'https://demo.cetmix.com',
     'live_test_url': 'https://demo.cetmix.com',
     'description': """
-    Preview reports and pdf attachments in browser instead of downloading them.
-    Open Report or PDF Attachment in new tab instead of downloading.              
+    Preview reports in browser instead of downloading them.
+    Open Report in new tab instead of downloading.              
 """,
     'depends': ['base', 'web'],
     'images': ['static/description/banner.png'],
